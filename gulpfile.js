a804cce84dca90c5342b93390f2c5db481229d52
Steren Giannini <steren@google.com>
2016-07-20 14:56:32 +0200

    generate source maps

--- gulpfile.js
+++ gulpfile.js
@@ -16,15 +16,18 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
 
 var DEST = 'dist/';
 
 gulp.task('default', function() {
   return gulp.src('stackdriver-errors.js')
+    .pipe(sourcemaps.init())
     // This will output the non-minified version
     .pipe(gulp.dest(DEST))
     // This will minify and rename to stackdriver-errors.min.js
     .pipe(uglify())
     .pipe(rename({ extname: '.min.js' }))
+    .pipe(sourcemaps.write('maps'))
     .pipe(gulp.dest(DEST));
 });
