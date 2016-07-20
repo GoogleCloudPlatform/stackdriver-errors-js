b2220769e6d586ab2cf552780e649ed3e82bc39f
Steren Giannini <steren@google.com>
2016-07-20 14:40:14 +0200

    add gulpfile to generate minified version

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+/**
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+
+var DEST = 'dist/';
+
+gulp.task('default', function() {
+  return gulp.src('stackdriver-errors.js')
+    // This will output the non-minified version
+    .pipe(gulp.dest(DEST))
+    // This will minify and rename to stackdriver-errors.min.js
+    .pipe(uglify())
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(gulp.dest(DEST));
+});
