91a563199d03ea47e9ec6ba2ddef2dd20077fc95
Steren Giannini <steren@google.com>
2016-12-01 13:32:50 +0100

    update Angular demo with exceptionHandler

--- demo/demo-angular.js
+++ demo/demo-angular.js
@@ -1,9 +1,21 @@
 angular.module('demoApp', [])
+
+  .factory('$exceptionHandler', ['$log', '$window', function($log, $window) {
+    var StackdriverErrors = new $window.StackdriverErrorReporter();
+    StackdriverErrors.start({
+      projectId: '<your-project-id>',
+      key: '<your-api-key>'
+    });
+
+    return function(exception, cause) {
+      StackdriverErrors.report(exception);
+      $log.warn('Reported error:', exception, cause);
+    };
+  }])
+
   .controller('DemoController', function() {
     var demo = this;
-    demo.customText = '';
- 
-    demo.triggerCrash = function() {
-      console.log('crash', demo.customText);
+    demo.starUser = function() {
+      demo.user.star();
     };
   });
