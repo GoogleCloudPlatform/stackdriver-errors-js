f3e7f43683b797bc17c9206fa71afa1c2dd3fc48
Martin Packman <martin@zegami.com>
2018-11-26 21:03:20 +0000

    Bundle dist script with browserify
    Change gulp dist task from concatting prebuilt source to building
    from components and bundling using browserify.
    
    Supply polyfills from core-js via browserify require, based on the
    upstream polyfills in stacktrace-js. This may be overly conservative
    as browsers back to IE 9 have good enough ES5 support, just filling
    Promise is probably be reasonable.

--- test/setup.js
+++ test/setup.js
@@ -1,11 +1,11 @@
 // Setup for tests to run outside of a browser environment
-/* eslint-disable no-global-assign,no-undef */
+/* global global */
 global.chai = require('chai');
 global.sinon = require('sinon');
 
 global.StackdriverErrorReporter = require('../stackdriver-errors');
 
-global.window = window = {
+global.window = {
   location: {href: 'http://stackdriver-errors.test/'},
   navigator: {userAgent: 'FakeAgent'},
 };
